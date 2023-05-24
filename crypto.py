8fd45de7a770285896522a38e3d2086199b8fe89
zKaiserJux <juliuseitz@gmx.de>
2023-05-24 22:27:11 +0200

    crucial misunderstanding of the key principle so fixed all the bugs and integrated the encryption of the encryption key

--- crypto.py
+++ crypto.py
@@ -4,52 +4,60 @@ import base64
 import hashlib
 import os
 
-# encrypts the master key and returns it decoded
-def encrypt_master_key(master_key : str):
+# hashes the master key and saves it in a file
+def hash_master_key(master_key : str, master_key_file : str):
     master_key = master_key.encode("utf-8")
     master_key = base64.b64encode(hashlib.sha256(master_key).digest())
     hashed = bcrypt.hashpw(master_key, bcrypt.gensalt())
+
+    # creates a file and saves the hashed master key bytes
+    with open(master_key_file, "wb") as file:
+        file.write(hashed)
+    
     return hashed
 
-# generates the encryption key and saves it in a file
-def generate_encryption_key(key_file : str):
+# generates the encryption key, encrypts it with a derived key and saves it in a file
+def generate_encryption_key(hashed_master_key : bytes, key_file : str):
+    # generates the encryption key to en- and decrypt the passwords later on
     key = Fernet.generate_key()
-    with open(key_file, "wb") as file:
-        file.write(key)
 
-# encrypts the encryption key an saves it in a file
-def encrypt_encryption_key(master_key : str, encryption_key : bytes, key_file : str):
-    # use the master key to derive the key for the encryption key
-    derived_key = bcrypt.kdf(master_key, salt=bcrypt.gensalt(), desired_key_bytes=32, rounds=100)
-    cipher_suite = Fernet(base64.b64encode(derived_key))
+    # generates the derived key form the hashed master key
+    derived_key = bcrypt.kdf(hashed_master_key, salt=bcrypt.gensalt(), desired_key_bytes=32, rounds=12)
+    cipher_suite = Fernet(base64.urlsafe_b64encode(derived_key))
 
-    # encrypt the encryption key
-    encrypted_encryption_key = cipher_suite.encrypt(encryption_key)
+    # the encryption key is encrypted with the derived key
+    encrypted_encryption_key = cipher_suite.encrypt(key)
 
-    # save the encrypted encryption key in a file
+    # save the derived key in an extra file called derived_key.txt"
+    with open('derived_key.txt', "wb") as file:
+        file.write(derived_key)
+
+    # save the enrypted encryption key in an extra file
     with open(key_file, "wb") as file:
         file.write(encrypted_encryption_key)
-    
-    return derived_key
-    
+
+    return derived_key, encrypted_encryption_key
+
 # decrypts the encryption key and returns it decoded
-def decrypt_encryption_key(master_key : bytes, key_file : str):
-    if os.path.isfile(key_file):
-        # if file with the master_key exists read the file and extract the encrypted encryption key
-        with open(key_file) as file:
-            encrypted_key = file.read().strip().encode("utf-8")
-        cipher_suite = Fernet(base64.urlsafe_b64encode(master_key))
+def decrypt_encryption_key(derived_key_file : str, encrypted_key : bytes):
+    # if file with the derived_key exists read the file and extract the encrypted encryption key
+    if os.path.isfile(derived_key_file):
+        with open(derived_key_file, "rb") as file:
+            derived_key = file.read()
+    
+        # use the derived key to decrypt the encrypted encryption key
+        cipher_suite = Fernet(base64.urlsafe_b64encode(derived_key))
         decrypted_encryption_key = cipher_suite.decrypt(encrypted_key)
         return decrypted_encryption_key
 
-# encrypts a passwort with the encryption key
-def encrypt_password(password : str, encryption_key : bytes):
-    cipher_suite = Fernet(encryption_key)
+# encrypts a passwort with the decrypted encryption key
+def encrypt_password(password : str, decrypted_encryption_key : bytes):
+    cipher_suite = Fernet(decrypted_encryption_key)
     encrypted_password = cipher_suite.encrypt(password.encode())
     return encrypted_password
 
-# decrypts a passwort with the encryption key
-def decrypt_password(encrypted_password : bytes, encryption_key : bytes):
-    cipher_suite = Fernet(encryption_key)
+# decrypts a passwort with the decrypted encryption key
+def decrypt_password(encrypted_password : bytes, decrypted_encryption_key : bytes):
+    cipher_suite = Fernet(decrypted_encryption_key)
     decrypted_password = cipher_suite.decrypt(encrypted_password)
     return decrypted_password.decode()
--- passwordmanager.py
+++ passwordmanager.py
@@ -4,7 +4,7 @@ from base64 import b64encode
 from hashlib import sha256
 import sys
 import os
-from crypto import encrypt_master_key, generate_encryption_key, encrypt_password, decrypt_password, generate_encryption_key
+from crypto import hash_master_key, generate_encryption_key, encrypt_password, decrypt_password, generate_encryption_key, generate_encryption_key, decrypt_encryption_key
 from bcrypt import checkpw
 
 class PasswordManger:
@@ -14,6 +14,8 @@ class PasswordManger:
         self.encryption_key_file = "encryption_key.txt"
         self.logged_in_status = False
         self.encryption_key = None
+        self.derived_key = None
+        self.decrypted_encryption_key = None
 
     # Lässt den User einen Master-Key beim ersten Starten der Applikation festlegen und speichert diesen anschließend in einer separaten Datei
     def set_masterkey(self):
@@ -22,28 +24,32 @@ class PasswordManger:
         if confirm_masterkey != self.master_key:
             print("Die eingegebenen Master-Keys stimmen nicht überein! \nBitte wiederholen Sie den Vorgang.")
             self.set_masterkey()
-        # encryption of the master key
-        self.master_key = encrypt_master_key(self.master_key)
-        
-        # Speicherung des Master-Keys in einer separaten Datei:
-        with open (self.master_key_file, 'w') as file:
-            file.write(self.master_key)
+        # Master-Key wird gehashed und in der Datei self.master_key_file gespeichert
+        self.master_key = hash_master_key(self.master_key, self.master_key_file)
+
+    # Gibt den gehashten Master-Key aus der Datei self.master_key_file zurück
+    def get_masterkey(self):
+        if os.path.isfile(self.master_key_file):
+            with open(self.master_key_file, "rb") as file:
+                self.master_key = file.read()
+                return self.master_key
+        else:
+            return None
 
     # Zugriff bei Bedarf auf den Master-Key -> Datei in der sich der Master-Key befindet wird ausegelesen und der Inhalt in self.master_key geladen
-    def get_masterkey(self):
+    def get_derived_key(self):
         # Wenn eine Datei "master_key.txt" existiert, wird diese im read-mode geöffnet und ausgelesen
-        if os.path.isfile(self.master_key_file):
-            with open(self.master_key_file, 'r') as file:
-                self.master_key = file.read().strip()
+        if os.path.isfile("derived_key.txt"):
+            with open("derived_key.txt", 'rb') as file:
+                self.derived_key = file.read()
+                return self.derived_key
         else:
             return None
         
-    # Holt sich den encryption key bei der Ver- und Entschlüsselung von Passwörtern
-    def get_encryption_key(self):
-        if os.path.isfile(self.encryption_key_file):
-            with open(self.encryption_key_file, "r") as file:
-                self.encryption_key = file.read().strip()
-        return self.encryption_key
+    # Gibt den entschlüsselten encryption zurück
+    def get_decrypted_encryption_key(self, encrypted_key):
+        self.decrypted_encryption_key = decrypt_encryption_key("derived_key.txt", encrypted_key)
+        return self.decrypted_encryption_key
         
     # Wird aufgerufen, wenn ein Master-Key bereits existiert und sich der User mit diesem anmleden muss
     def user_login(self):
@@ -56,7 +62,7 @@ class PasswordManger:
             user_input = getpass.getpass("Bitte geben Sie den Master-Key ein, um sich einzuloggen: ")
             user_input = user_input.encode("utf-8")
             user_input = b64encode(sha256(user_input).digest())
-            if checkpw(user_input, self.master_key.encode("utf-8")):
+            if checkpw(user_input, self.master_key):
                 self.logged_in_status = True
                 print("[+] Login successfull.")
                 break
@@ -72,33 +78,32 @@ class PasswordManger:
             
     # Fragt den Nutzer nach einem neuen Account und erhält die Login-Daten mit samt der URL
     def add_credentials(self):
-        self.encryption_key = self.get_encryption_key().encode("utf-8")
         url = input("* Name / URL: ")
         email_username = input("* email/username: ")
         password = getpass.getpass("* Password: ")
-        password = encrypt_password(password, self.encryption_key)
 
+        # Holen des entschlüsselten encryption keys mit dem die Passwörter verschlüsselt werden
+        # self.decrypted_encryption_key = self.get_decrypted_encryption_key()
+        password = encrypt_password(password, self.decrypted_encryption_key)
+
+        # Verbinden mit der Datenbank, um die eingebenen credentials in die Datenbank aufzunehmen
         conn = sqlite3.connect("passwordmanager.db")
         cur = conn.cursor()
 
-         # SQL-Befehl zum Einfügen eines neuen Eintrages in die Tabelle
-        insert_query = '''
-        INSERT INTO passwords (url, email_username, password)
-        VALUES (?, ?, ?)
-        '''
-        # Eintrag in die Tabelle einfügen
-        cur.execute(insert_query, (url, email_username, password))
+        # Credentials werden in die Tabelle eingefügt
+        cur.execute("INSERT INTO passwords (url, email_username, password) VALUES (?, ?, ?)", (url, email_username, password))
         conn.commit()
 
+        # Verbindung mit der Datenbank wird beendet
         conn.close()
 
-    # Datenbank, die die Website als URL speichert, den Usernamen/E-Mail für einen Account und das dazugehörige Passwort
+    # Initialisierung der Datenbank, die später die Credentials aller Online-Accounts speichert
     def initialise_db(self):
         # Wenn keine master_key Datei existiert, wird set_masterkey ausgeführt
         if not os.path.isfile(self.master_key_file):
             self.set_masterkey()
-            # key zum Ver- und Entschlüsseln der Passwörter wird initialisiert
-            generate_encryption_key(self.encryption_key_file)
+            # key zum Ver- und Entschlüsseln der Passwörter wird generiert
+            # generate_encryption_key(self.master_key, self.encryption_key_file)
 
         # Wenn ja, dann muss sich der User einloggen
         else:
@@ -109,7 +114,8 @@ class PasswordManger:
         cur = conn.cursor()
 
         # Der key wird mit dem Master-Key verschlüsselt und in der Datei "encryption_key.txt" gespeichert
-        # self.encryption_key = encrypt_encryption_key(self.master_key, self.encryption_key, self.encryption_key_file)
+        self.derived_key, self.encryption_key = generate_encryption_key(self.master_key, self.encryption_key_file)
+        self.decrypted_encryption_key = self.get_decrypted_encryption_key(self.encryption_key)
 
         # SQL-Befehl zum Erstellen der Datentabelle
         create_table_query = '''
@@ -142,8 +148,7 @@ class PasswordManger:
         # Falls eine Zeile mit Daten gefunden wurde, werden die credentials für den Account ausgegeben
         if result is not None:
             # self.encryption_key = decrypt_encryption_key(self.master_key.encode("utf-8"), self.encryption_key_file)
-            self.encryption_key = self.get_encryption_key()
-            password = decrypt_password(result[1], self.encryption_key)
+            password = decrypt_password(result[1], self.decrypted_encryption_key)
             print(f"[+] Benutzername: {result[0]}, Passwort: {password}")
 
         # Wenn kein Eintrag gefunden werden konnte, wird dem Nutzer ein entsprechender Hinweis ausgegeben
